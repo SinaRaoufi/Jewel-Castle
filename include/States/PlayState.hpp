723861c890f5c1b84299b7fe9e8fe402b9209a94
SinaRaoufi <sinaraoufi@outlook.com>
2020-07-27 15:36:00 +0430

    implement evenHandler member function of class PlayState

--- include/States/PlayState.hpp
+++ include/States/PlayState.hpp
@@ -6,7 +6,7 @@
 class PlayState : public GameState
 {
 public:
-PlayState();
+    PlayState();
     virtual GameState *eventHandler(sf::RenderWindow &, StateList &, sf::Event &) override;
     virtual GameState *update(sf::RenderWindow &, StateList &) override;
     virtual void render(sf::RenderWindow &) override;
--- src/MainMenuState.cpp
+++ src/MainMenuState.cpp
@@ -1,4 +1,5 @@
 #include "States/MainMenuState.hpp"
+#include "States/PlayState.hpp"
 
 using namespace std;
 
@@ -10,6 +11,14 @@ MainMenuState::MainMenuState()
 
 GameState *MainMenuState::eventHandler(sf::RenderWindow &window, StateList &state, sf::Event &event)
 {
+    if (event.type == sf::Event::KeyPressed)
+        if (event.key.code == sf::Keyboard::Space)
+        {
+            if (state[1] != nullptr)
+                delete state[1];
+            state[1] = new PlayState();
+            return state[1];
+        }
     return this;
 }
 GameState *MainMenuState::update(sf::RenderWindow &window, StateList &state)
