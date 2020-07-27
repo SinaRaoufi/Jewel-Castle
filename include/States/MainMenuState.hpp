af56be624502362d844e01309e15176babb3c6b0
SinaRaoufi <sinaraoufi@outlook.com>
2020-07-27 12:00:52 +0430

    implement constructor of MainMenuState class

---
+++ include/States/MainMenuState.hpp
@@ -0,0 +1,12 @@
+#ifndef MAIN_MENU_STATE_IG
+#define MAIN_MENU_STATE_IG
+
+#include "States/GameState.hpp"
+
+class MainMenuState : public GameState
+{
+    public:
+    MainMenuState();
+};
+
+#endif
---
+++ src/MainMenuState.cpp
@@ -0,0 +1,9 @@
+#include "States/MainMenuState.hpp"
+
+using namespace std;
+
+MainMenuState::MainMenuState()
+{
+    backgroundPath = "main_menu.png";
+    setBackground();
+}
