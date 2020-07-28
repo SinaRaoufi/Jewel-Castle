378ee713a7b0e7d43637ab161c5edb3ce604326e
SinaRaoufi <sinaraoufi@outlook.com>
2020-07-28 12:21:54 +0430

    add some comment to configs.hpp

--- include/configs.hpp
+++ include/configs.hpp
@@ -1,11 +1,15 @@
 #ifndef CONFIGS_IG
 #define CONFIGS_IG
 
+// Window Settings
 #define WINDOW_HEIGHT 530
 #define WINDOW_WIDTH 600
 #define WINDOW_TITLE "Jewel Castle"
 
+// States Settings
 #define NUMBER_OF_STATES 5
+
+// Directory Settings
 #define BACKGROUND_DIRECTORY "../assets/backgrounds/" 
 #define FONT_DIRECTORY "../assets/fonts/" 
 
