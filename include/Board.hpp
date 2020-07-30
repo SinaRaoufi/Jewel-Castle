01ef732b88018bd96045b193122ed0cbbc80a192
SinaRaoufi <sinaraoufi@outlook.com>
2020-07-30 12:05:40 +0430

    add generateRandomNumber and generateRandomJewels member functions

--- include/Board.hpp
+++ include/Board.hpp
@@ -4,9 +4,12 @@
 #include <iostream>
 #include <array>
 #include "Tile.hpp"
+#include "Jewels/Jewel.hpp"
 #include "configs.hpp"
 
 typedef std::array<std::array<Tile *, NUM_OF_COLUMN>, NUM_OF_ROW> TileList;
+typedef std::array<std::array<Jewel *, NUM_OF_COLUMN>, NUM_OF_ROW> JewelList;
+typedef unsigned short int us;
 
 class Board
 {
@@ -16,12 +19,15 @@ public:
     size_t getNumberOfRow() const;
     size_t getNumberOfColumn() const;
     TileList getListOfTiles() const;
+    void generateRandomJewels(JewelList &);
     void render(sf::RenderWindow &);
 
 private:
     const size_t numberOfRow = NUM_OF_ROW;
     const size_t numberOfColumn = NUM_OF_COLUMN;
     TileList listOfTiles;
+    JewelList listOfJewels;
+    us generateRandomNumber(us, us); // utility member function for generating random number
 };
 
 #endif
--- src/Board.cpp
+++ src/Board.cpp
@@ -1,19 +1,39 @@
 #include <Board.hpp>
+#include <ctime>
+#include <random>
 #include <SFML/Graphics.hpp>
+
+#include "Jewels/YellowJewel.hpp"
+#include "Jewels/GreenJewel.hpp"
+#include "Jewels/RedJewel.hpp"
+#include "Jewels/BlueJewel.hpp"
+#include "Jewels/PinkJewel.hpp"
+#include "Jewels/VioletJewel.hpp"
+
+using namespace std;
 
 Board::Board()
 {
+    generateRandomJewels(listOfJewels);
     for (size_t i = 0; i < numberOfRow; i++)
         for (size_t j = 0; j < numberOfColumn; j++)
         {
-            listOfTiles[i][j] = new Tile(Tile_WIDTH, Tile_HEIGHT, sf::Color::Blue, sf::Color::Black);
+            sf::Color tileColor(216, 191, 216);
+            listOfTiles[i][j] = new Tile(Tile_WIDTH, Tile_HEIGHT, tileColor, sf::Color::Black);
             listOfTiles[i][j]->setTilePosition(j * Tile_WIDTH + 150, Tile_HEIGHT * i + 150);
+            listOfJewels[i][j]->setJewelPosition(j * Tile_WIDTH + 150, Tile_HEIGHT * i + 150);
         }
 }
 
 Board::~Board()
 {
+    // delete each tile
     for (auto &row : listOfTiles)
+        for (auto &item : row)
+            delete item;
+
+    //delete each jewel
+    for (auto &row : listOfJewels)
         for (auto &item : row)
             delete item;
 }
@@ -21,6 +41,10 @@ Board::~Board()
 void Board::render(sf::RenderWindow &window)
 {
     for (auto row : listOfTiles)
+        for (auto item : row)
+            item->render(window);
+
+    for (auto row : listOfJewels)
         for (auto item : row)
             item->render(window);
 }
@@ -38,4 +62,51 @@ size_t Board::getNumberOfColumn() const
 TileList Board::getListOfTiles() const
 {
     return listOfTiles;
+}
+
+// utility member function for generating random number
+us Board::generateRandomNumber(us range1, us range2)
+{
+    random_device randomDevice;
+    mt19937 mt(randomDevice());
+    uniform_int_distribution<us> randomID(range1, range2);
+    return randomID(mt);
+}
+
+void Board::generateRandomJewels(JewelList &listOfJewels)
+{
+    enum JewelsEnum
+    {
+        YELLOW_JEWEL,
+        GREEN_JEWEL,
+        RED_JEWEL,
+        BLUE_JEWEL,
+        PINK_JEWEL,
+        VIOLET_JEWEL
+    };
+    for (size_t i = 0; i < numberOfRow; i++)
+        for (size_t j = 0; j < numberOfColumn; j++)
+        {
+            switch (generateRandomNumber(YELLOW_JEWEL, VIOLET_JEWEL))
+            {
+            case YELLOW_JEWEL:
+                listOfJewels[i][j] = new YellowJewel();
+                break;
+            case GREEN_JEWEL:
+                listOfJewels[i][j] = new GreenJewel();
+                break;
+            case RED_JEWEL:
+                listOfJewels[i][j] = new RedJewel();
+                break;
+            case BLUE_JEWEL:
+                listOfJewels[i][j] = new BlueJewel();
+                break;
+            case PINK_JEWEL:
+                listOfJewels[i][j] = new PinkJewel();
+                break;
+            case VIOLET_JEWEL:
+                listOfJewels[i][j] = new VioletJewel();
+                break;
+            }
+        }
 }
