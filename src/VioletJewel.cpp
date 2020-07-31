232f6ec3b1f3785b63a7a894c29132f31c615f0e
SinaRaoufi <sinaraoufi@outlook.com>
2020-07-31 19:34:18 +0430

    change shape of violet jewel

--- src/VioletJewel.cpp
+++ src/VioletJewel.cpp
@@ -4,6 +4,7 @@ VioletJewel::VioletJewel() : Jewel(VIOLET_JEWEL_SCORE)
 {
     // set violet jewel properties
     this->jewelShape.setRadius(20.f);
+    this->jewelShape.setPointCount(6);
     sf::Color violetColor(148, 0, 211);
     this->jewelShape.setFillColor(violetColor);
     this->jewelShape.setOutlineThickness(1);
