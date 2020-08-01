109e7f81efe09044f8b0479a8a7a970f7a400504
SinaRaoufi <sinaraoufi@outlook.com>
2020-08-01 15:21:42 +0430

    change shape of Red Jewel

--- src/RedJewel.cpp
+++ src/RedJewel.cpp
@@ -4,6 +4,7 @@ RedJewel::RedJewel() : Jewel(RED_JEWEL_SCORE)
 {
     // set red jewel properties
     this->jewelShape.setRadius(20.f);
+    this->jewelShape.setPointCount(7);
     this->jewelShape.setFillColor(sf::Color::Red);
     this->jewelShape.setOutlineThickness(1);
     this->jewelShape.setOutlineColor(sf::Color::Cyan);
