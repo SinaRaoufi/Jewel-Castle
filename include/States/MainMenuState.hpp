33f1dc7841cf75f052ec23d91580d6263f5960b1
SinaRaoufi <sinaraoufi@outlook.com>
2020-08-08 17:18:20 +0430

    add Exit button to main menu

--- include/States/MainMenuState.hpp
+++ include/States/MainMenuState.hpp
@@ -14,6 +14,7 @@ public:
 
 private:
     Button playButton;
+    Button exitButton;
 };
 
 #endif
--- src/MainMenuState.cpp
+++ src/MainMenuState.cpp
@@ -4,23 +4,17 @@
 
 using namespace std;
 
-MainMenuState::MainMenuState() : playButton(BUTTON_TEXTURE_DIRECTORY +string("play_button.png"))
+MainMenuState::MainMenuState() : playButton(BUTTON_TEXTURE_DIRECTORY +string("play_button.png")),
+exitButton(BUTTON_TEXTURE_DIRECTORY +string("exit_button.png"))
 {
     playButton.setButtonPosition(250,200);
+    exitButton.setButtonPosition(250,300);
     backgroundPath = "main_menu.jpg";
     setBackground();
 }
 
 GameState *MainMenuState::eventHandler(sf::RenderWindow &window, StateList &state, sf::Event &event)
 {
-    // if (event.type == sf::Event::KeyPressed)
-    //     if (event.key.code == sf::Keyboard::Space)
-    //     {
-    //         if (state[PLAY] != nullptr)
-    //             delete state[PLAY];
-    //         state[PLAY] = new PlayState();
-    //         return state[PLAY];
-    //     }
     if (event.type == sf::Event::MouseButtonPressed)
         if (event.mouseButton.button == sf::Mouse::Left)
             if (playButton.isButtonPressed(sf::Vector2f(event.mouseButton.x, event.mouseButton.y)))
@@ -40,4 +34,5 @@ void MainMenuState::render(sf::RenderWindow &window)
 {
     window.draw(backgroundSprite);
     playButton.render(window);
+    exitButton.render(window);
 }
