4ec6fc5776cd45484ec1078a19f8a7a81b981593
SinaRaoufi <sinaraoufi@outlook.com>
2020-08-16 12:07:03 +0430

    implement exitButton event for MainMenuState

--- src/MainMenuState.cpp
+++ src/MainMenuState.cpp
@@ -4,11 +4,11 @@
 
 using namespace std;
 
-MainMenuState::MainMenuState() : playButton(BUTTON_TEXTURE_DIRECTORY +string("play_button.png")),
-exitButton(BUTTON_TEXTURE_DIRECTORY +string("exit_button.png"))
+MainMenuState::MainMenuState() : playButton(BUTTON_TEXTURE_DIRECTORY + string("play_button.png")),
+                                 exitButton(BUTTON_TEXTURE_DIRECTORY + string("exit_button.png"))
 {
-    playButton.setButtonPosition(250,200);
-    exitButton.setButtonPosition(250,300);
+    playButton.setButtonPosition(250, 200);
+    exitButton.setButtonPosition(250, 300);
     backgroundPath = "main_menu.jpg";
     setBackground();
 }
@@ -24,6 +24,10 @@ GameState *MainMenuState::eventHandler(sf::RenderWindow &window, StateList &stat
                 state[PLAY] = new PlayState();
                 return state[PLAY];
             }
+    if (event.type == sf::Event::MouseButtonPressed)
+        if (event.mouseButton.button == sf::Mouse::Left)
+            if (exitButton.isButtonPressed(sf::Vector2f(event.mouseButton.x, event.mouseButton.y)))
+                window.close();
     return this;
 }
 GameState *MainMenuState::update(sf::RenderWindow &window, StateList &state)
