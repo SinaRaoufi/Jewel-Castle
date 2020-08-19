b2f669e5168de2bdaada6c076108c67d44efa680
SinaRaoufi <sinaraoufi@outlook.com>
2020-08-19 16:19:39 +0430

    add comments to RandomNumberGenerator source file

--- src/RandomNumberGenerator.cpp
+++ src/RandomNumberGenerator.cpp
@@ -6,8 +6,8 @@ using namespace std;
 // function for generating random number
 us generateRandomNumber(us range1, us range2)
 {
-    random_device randomDevice; //  uniform distribution
+    random_device randomDevice;
     mt19937 mt(randomDevice()); // seed the mt19937 generator using randomDevice
     uniform_int_distribution<us> randomID(range1, range2);
-    return randomID(mt);
+    return randomID(mt); // return random number from range1 to range 2
 }
